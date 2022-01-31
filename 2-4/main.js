c457a672c596fc56fd0aa329963120a44794aea1
権藤　崚志 <ryoji@noMacBook-Air.local>
2022-01-31 13:16:48 +0900

    2-4

---
+++ 2-4/main.js
@@ -0,0 +1,24 @@
+const season = Number( process.argv[2] )
+
+switch (season) {
+  case 2:
+  case 3:
+  case 4:
+    console.log("春");
+    break;
+  case 5:
+  case 6:
+  case 7:
+    console.log("夏");
+    break;
+  case 8:
+  case 9:
+  case 10:
+    console.log("秋");
+    break;
+  case 1:
+  case 11:
+  case 12:
+    console.log("冬");
+    break;
+}
