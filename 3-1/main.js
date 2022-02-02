24f6381d4b694b3df62592c4ea2fba7bbbff9429
権藤　崚志 <ryoji@noMacBook-Air.local>
2022-02-03 02:14:08 +0900

    3-1

--- 3-1/main.js
+++ 3-1/main.js
@@ -1,10 +1,7 @@
-let array_x = [1,2,3,4,5,6,7,8,9]
-let array_y = [1,2,3,4,5,6,7,8,9]
-
-for ( let x = 0; x < array_x.length; x++) {
+for ( let x = 1; x < 10; x++) {
   let array_kuku = [];
-  for ( let y = 0; y < array_y.length; y++) {
-    let array_dan = (array_x[x] * array_y[y])
+  for ( let y = 1; y < 10; y++) {
+    let array_dan = (x * y)
     array_kuku.push(array_dan);
   }
   console.log(array_kuku.join(''));
