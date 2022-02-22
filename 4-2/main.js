a81ac627ae3c29a8da9d705e8d8aea48231c83ce
権藤　崚志 <ryoji@noMacBook-Air.local>
2022-02-23 02:47:46 +0900

    4-2

--- 4-2/main.js
+++ 4-2/main.js
@@ -1,5 +1,5 @@
 for (let i = 0; i <= 50; i++) {
-  if ( i % 3 === 0 || /3/.test(i)) { 
+  if ( i % 3 === 0 || i % 10 === 3 || Math.floor(i / 10) === 3) {
     console.log("WOW");
   } else {
     console.log(i)
