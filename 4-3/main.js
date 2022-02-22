ddd46d978e8ad16adc63a1b25fa512cbf2761831
権藤　崚志 <ryoji@noMacBook-Air.local>
2022-02-23 01:35:42 +0900

    4-3

--- 4-3/main.js
+++ 4-3/main.js
@@ -3,3 +3,9 @@ const errorLogs = [
   "Error 3:Invalid Exception BBBB - 2020/01/01/02:00",
   "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00"
 ]
+
+for ( let i = 0; i < errorLogs.length; i++) {
+  let logNumber = errorLogs[i]
+  let errorDetails = logNumber.split(/[:-]/)
+  console.log(errorDetails[1]);
+}
