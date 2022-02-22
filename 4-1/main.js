40a9c4768b6cc71c101d6f967f006fe133788c7c
権藤　崚志 <ryoji@noMacBook-Air.local>
2022-02-23 01:51:06 +0900

    4-1

--- 4-1/main.js
+++ 4-1/main.js
@@ -1,17 +1,16 @@
-const numbers = [process.argv[2],process.argv[3],process.argv[4],process.argv[5],process.argv[6]];
-
-let list = [];
-numbers.forEach(function(number) {
-  list.push(number)
-})
+const arr = [];
+let args = process.argv.slice(2);
+  for (let i = 0; i < args.length; i++) {
+    arr.push(Number(args[i]));
+  }
 
 function compareNum (a, b) {
   return a - b;
 }
-list.sort(compareNum);
+arr.sort(compareNum);
 
-if ( list.indexOf(undefined) === -1 ) {
-  console.log(list[2]);
+if ( arr.indexOf(undefined) === -1 ) {
+  console.log(arr[2]);
 } else {
   console.log("5つの整数を入力してください");
 }
